097de28ba6a9f3102ac2ef3be819786cc12e8a14
pooderds <pri198r@gmail.com>
2023-02-17 14:17:41 +0300

    и

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
 
 //All Authors Route
 router.get('/', (req, res) => {
@@ -8,12 +9,12 @@ router.get('/', (req, res) => {
 
 //New Author Route
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', {author: new Author() })
 })
 
 //Create Author Route
 router.post('/', (req, res) => {
-    res.send('Create')
+    res.send(req.body.name)
 })
 
 
